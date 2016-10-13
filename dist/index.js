788aaecf6fb5131bf350c5e2f0c21fb52b43d623
Mauricio <mauriciolealmx@gmail.com>
2016-10-13 11:27:54 -0500

    Creating a dist dir with babel-cli

---
+++ dist/index.js
@@ -0,0 +1,24 @@
+var cool = require('cool-ascii-faces');
+var express = require('express');
+var app = express();
+var path = require('path');
+
+app.set('port', process.env.PORT || 5000);
+
+app.use(express.static(path.join(__dirname, '../public')));
+
+// views is directory for all template files
+app.set('views', path.join(__dirname, '../views'));
+app.set('view engine', 'ejs');
+
+app.get('/', function (request, response) {
+  response.render('pages/index');
+});
+
+app.get('/cool', function (request, response) {
+  response.send(cool());
+});
+
+app.listen(app.get('port'), function () {
+  console.log('Node app is running on port', app.get('port'));
+});
