4538f49f1f73bff0ad77eee89caade5a7d9a07ef
Mauricio <mauriciolealmx@gmail.com>
2016-10-20 16:00:42 -0500

    Facebook login button working locally

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports={
+  "facebook_api_key" : "543197432545471",
+  "facebook_api_secret" : "4109f5b5114f59a05eae554f3e678dea",
+  "callback_url" : "http://localhost:5000/auth/facebook/callback",
+  "use_database" : "false",
+  "host" : "localhost",
+  "username" : "root",
+  "password" : "",
+  "database" : "DB NAME"
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,46 @@
-var cool = require('cool-ascii-faces');
-var express = require('express');
-var app = express();
-var path = require('path');
+var cool = require('cool-ascii-faces'),
+  passport = require('passport'),
+  FacebookStrategy = require('passport-facebook').Strategy,
+  session = require('express-session'),
+  cookieParser = require('cookie-parser'),
+  bodyParser = require('body-parser'),
+  express = require('express'),
+  app = express(),
+  config = require('../config'),
+  path = require('path');
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
+// Use the FacebookStrategy within Passport.
+passport.use(new FacebookStrategy({
+    clientID: config.facebook_api_key,
+    clientSecret:config.facebook_api_secret ,
+    callbackURL: config.callback_url,
+    profileFields: ['id', 'emails', 'name', 'gender', 'profileUrl', 'photos']
+  }, function(accessToken, refreshToken, profile, done) {
+    process.nextTick(function () {
+      //Check whether the User exists or not using profile.id
+      if(config.use_database==='true') {
+        connection.query("SELECT * from user_info where user_id="+profile.id,function(err,rows,fields){
+          if(err) throw err;
+          if(rows.length===0) {
+            console.log("There is no such user, adding now");
+            connection.query("INSERT into user_info(user_id,user_name) VALUES('"+profile.id+"','"+profile.username+"')");
+          } else {
+            console.log("User already exists in database");
+          }
+        });
+      }
+      return done(null, profile);
+    });
+  }
+));
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -10,9 +49,35 @@ app.use(express.static(path.join(__dirname, '../public')));
 // views is directory for all template files
 app.set('views', path.join(__dirname, '../views'));
 app.set('view engine', 'ejs');
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(session({ secret: 'keyboard cat', key: 'sid'}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.get('/', function(request, response) {
   response.render('pages/index');
+});
+
+app.get('/auth/facebook', passport.authenticate('facebook',{scope:'email'}));
+
+app.get('/auth/facebook/callback',
+  passport.authenticate('facebook', { successRedirect : '/checarDatos', failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/');
+});
+
+app.get('/checarDatos', function(req, res){
+
+  console.log(`
+    id: ${req.user.id}
+    name: ${req.user.name.givenName}
+    middle name: ${req.user.name.middleName}
+    family name: ${req.user.name.familyName}
+    emails: ${req.user.emails[0].value}
+    provider: ${req.user.provider}
+  `)
+  res.redirect('/');
 });
 
 app.get('/cool', function(request, response) {
