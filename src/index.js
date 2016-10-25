28e92d9038754388f10fcd31d3c6d44992a99da1
Mauricio <mauriciolealmx@gmail.com>
2016-10-25 16:55:14 -0500

    Installing Postgres 2

--- src/index.js
+++ src/index.js
@@ -7,7 +7,21 @@ var cool = require('cool-ascii-faces'),
   express = require('express'),
   app = express(),
   config = require('../config'),
-  path = require('path');
+  path = require('path'),
+  pg = require('pg');
+  
+
+app.get('/db', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM test_table', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.render('pages/db', {results: result.rows} ); }
+    });
+  });
+});;
 
 passport.serializeUser(function(user, done) {
   done(null, user);
