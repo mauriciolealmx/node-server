4ce9746789d2f8ae597e965a83406771ba96478b
Mauricio Leal <Mauricio.AndresLealVillarael@classmates.com>
2017-02-14 17:59:26 -0600

    Adding get to api/users

--- src/index.js
+++ src/index.js
@@ -87,6 +87,7 @@ app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
+// Verify functionallity with curl like so: curl --data "name=test" http://127.0.0.1:5000/api/users
 app.post('/api/users', (req, res, next) => {
   const results = [];
   // Grab data from http request
@@ -103,6 +104,30 @@ app.post('/api/users', (req, res, next) => {
     client.query('INSERT INTO users(name) values($1)', [data.name]);
     // SQL Query > Select Data
     const query = client.query('SELECT * FROM users ORDER BY id ASC');
+    // Stream results back one row at a time
+    query.on('row', (row) => {
+      results.push(row);
+    });
+    // After all data is returned, close connection and return results
+    query.on('end', () => {
+      done();
+      return res.json(results);
+    });
+  });
+});
+
+router.get('/api/users', (req, res, next) => {
+  const results = [];
+  // Get a Postgres client from the connection pool
+  pg.connect(connectionString, (err, client, done) => {
+    // Handle connection errors
+    if(err) {
+      done();
+      console.log(err);
+      return res.status(500).json({success: false, data: err});
+    }
+    // SQL Query > Select Data
+    const query = client.query('SELECT * FROM users ORDER BY id ASC;');
     // Stream results back one row at a time
     query.on('row', (row) => {
       results.push(row);
